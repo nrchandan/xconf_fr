e3faca7a0638914e81906076f8ba76b36ddf5191
Chandan Kumar <kchandan@thoughtworks.com>
2018-12-13 12:03:13 +0530

    Remove dependency on zsh [WIP]
    Not tested yet.

---
+++ activate.sh
@@ -0,0 +1,4 @@
+export PATH="/usr/local/share/miniconda3/bin:$PATH"
+. /usr/local/share/miniconda3/etc/profile.d/conda.sh
+export PATH="/usr/local/share/torch/install/bin:$PATH"
+. /usr/local/share/torch/install/bin/torch-activate
--- install.sh
+++ install.sh
@@ -7,20 +7,12 @@ fi
 if ! which wget > /dev/null; then
   brew install wget
 fi
-if ! which zsh > /dev/null; then
-	brew install zsh
-fi
 if ! ls miniconda.sh > /dev/null; then
 	wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ./miniconda.sh
 fi
 sh miniconda.sh -p /usr/local/share/miniconda3/ -b -f
-echo 'export PATH="/usr/local/share/miniconda3/bin:$PATH"' >> ~/.zshrc
-echo '. /usr/local/share/miniconda3/etc/profile.d/conda.sh' >> ~/.zshrc
-source ~/.zshrc
 tar -C /usr/local/share -xvf torch.tgz
-echo 'export PATH="/usr/local/share/torch/install/bin:$PATH"' >> ~/.zshrc
-echo '. /usr/local/share/torch/install/bin/torch-activate' >> ~/.zshrc
-source ~/.zshrc
+./activate.sh
 luarocks install dpnn
 conda create -y -n xconf python==3.5
 conda activate xconf
